3398a0353f236e35fd60ec1ac554038d4e07699b
Hamza MARE <batchiy6@gmail.com>
2021-11-07 01:32:09 -0500

    retouch

--- DOM/todo_app/todoApp.js
+++ DOM/todo_app/todoApp.js
@@ -4,18 +4,32 @@ const form = document.querySelector('form');
 
 const lastSection = document.querySelector('#lastSection')
 
-const newbutton = document.createElement('button');
-
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-    const newP = document.createElement("p");
-    newP.innerHTML = `${text_input.value} <button class="remove">Remove</button>`
-    lastSection.appendChild(newP);
-    newP.classList.add('newTask')
+    if (text_input.value === "") {
+        alert("You can't submit an empty field")
+    } else {
+        const newP = document.createElement("p");
+        newP.innerText = text_input.value
+        newP.classList.add('newTask')
+        
+        const newButton = document.createElement('button')
+        newButton.innerText = 'remove';
+        newButton.classList.add('remove');
+    
+        lastSection.appendChild(newP);
+        newP.appendChild(newButton);
+    }
     text_input.value = "";
+})
 
-    newP.addEventListener('click', function(e) {
-        e.target.classList.toggle('toggle');
-    })
+
+
+document.querySelector("#lastSection").addEventListener('click', function(e){
+    if (e.target.tagName.toLowerCase() === 'p'){
+        e.target.classList.toggle('toggle')
+    } else if (e.target.tagName.toLowerCase() === 'button') {
+        e.target.parentNode.remove();
+    }
 })
